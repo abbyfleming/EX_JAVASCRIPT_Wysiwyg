587add9aec949c6c9632791bb7f55d5a48ed9acf
Abby Moore <abbyleighanne@gmail.com>
2016-11-04 19:27:58 -0500

    event listeners working, add img

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ var famousPeople = [
 	title: "Inventor",
 	name: "Mary Anderson",
 	bio: "Mary Anderson was an American real estate developer, rancher, viticulturist and inventor of the windshield wiper blade.",
-	image: "http://womeninventors.weebly.com/uploads/1/8/6/9/18698464/1366038132.png",
+	image: "img/mary_anderson.jpg",
 	lifespan: {
 		birth: 1866,
 		death: 1953
@@ -15,7 +15,7 @@ var famousPeople = [
 	title: "Inventor & Chemist",
 	name: "Hazel Bishop",
 	bio: "Hazel Bishop is the inventor of smear-proof lipstick. In 1971, Hazel Bishop became the first female member of the Chemists' Club in New York.",
-	image: "http://i273.photobucket.com/albums/jj238/beckyingj/Hazel%20Bishop/bishopformal.jpg",
+	image: "img/hazel_bishop.jpg",
 	lifespan: {
 		birth: 1906,
 		death: 1998
@@ -26,7 +26,7 @@ var famousPeople = [
 	title: "Chemist",
 	name: "Marie Curie",
 	bio: "Marie Curie pioneered radioactivity research. She was the first two-time Nobel laureate and the only person to win the award in two different sciences (Linus Pauling won Chemistry and Peace).",
-	image: "https://cdn.kastatic.org/ka-perseus-images/763be879b7d74e6d7212b914d7b828d5c58bf504.jpg",
+	image: "img/marie_curie.jpg",
 	lifespan: {
 		birth: 1867,
 		death: 1934
@@ -37,7 +37,7 @@ var famousPeople = [
 	title: "Astronomer",
 	name: "Caroline Herschel",
 	bio: "Caroline became a brilliant astronomer in her own right, discovering new nebulae and star clusters. She was the first woman to discover a comet (she discovered eight in total) and the first to have her work published by the Royal Society.",
-	image: "http://thumbs.media.smithsonianmag.com//filer/women-scientists-Caroline-Herschel-2.jpg__800x0_q85_crop.jpg",
+	image: "img/caroline_herschel.jpg",
 	lifespan: {
 		birth: 1750,
 		death: 1848
